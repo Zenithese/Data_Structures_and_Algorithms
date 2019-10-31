45df6d7ef79201288314f99a10b5a9bbf1a837b6
Justin <jandersen770@gmail.com>
2019-10-31 11:21:43 -0400

    recursive ruby hash traversal

---
+++ recursive_hash.rb
@@ -0,0 +1,11 @@
+$count = 0
+
+def recursive_hash_traversal(hash)
+    $count += 1
+    hash.each{ |key, value| value.is_a?(Hash) ? recursive_hash_traversal(value) : value }
+end
+
+hash = { a: { b: {c: 'd'}}}
+recursive_hash_traversal(hash)
+
+puts $count # returns 3 (the number of hashes included in 'hash')
