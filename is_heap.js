44e639a4887afd916521316d00f6e945b7d4e455
Justin <jandersen770@gmail.com>
2019-11-01 12:07:51 -0400

    is heap function

--- is_heap.js
+++ is_heap.js
@@ -1,6 +1,6 @@
 function isMaxHeap(array, idx = 1) {
     if (array[idx] === undefined) return true;
-    if (array.length === idx) return true;
+    if (array.length - 1 === idx) return true;
 
     parent = array[idx];
     leftChild = array[idx * 2] || -Infinity;
